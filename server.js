a4d980fd8001a4cf7dd0ebbfce46e80820a4a9e5
Daniel Lim <hbt@markhamboards.com>
2016-09-11 12:30:41 -0400

    clean up
    clean up

--- server.js
+++ server.js
@@ -3,64 +3,32 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-// app.use(express.static('../public')); // path to your public directory
+const http = require('http').Server(app);
+const path = require('path');
+const moment = require('moment');
 
 app.use(express.static('/'));
-// This responds a GET request for the / page.
 app.get('/', function (req, res) {
-   // console.log("Got a GET request for /");
-   // res.send('Page Listing');
    fs.readFile(__dirname + '/views/index.html', 'utf8', function(err, text){
       res.send(text);
     });
 })
-
-// app.use(express.static(__dirname + "/index.html"));
-
-const http = require('http').Server(app);
-const path = require('path');
-
 app.set('port', (process.env.PORT || 8081));
-
-const moment = require('moment');
-
-// app.use(express.static(__dirname + '/index.html'));
-// app.use(express.static(__dirname));
-
-// app.use(express.static(__dirname + "/index.html"));
-
-/*
-app.get('/',function(req,res) {
-  res.render('index.html', {});
-});
-*/
-
-/*
-app.get('/', function (req, res) {
-  res.render('index', {});
-});
-*/
-
 app.get('/:date', (req,res) => {
-
   let time = moment(req.params.date, 'MMMM DD, YYYY', true);
-
   if (!time.isValid())
     time = moment.unix(req.params.date);
-
   if (!time.isValid()) {
     res.json({
       'unix': null,
       'natural': null
     });
   }
-
   res.json({
     'unix': time.format('X'),
     'natural': time.format('MMMM DD, YYYY')
   });
 });
-
 app.listen(app.get('port'), () => {
   console.log(`Server listening on port ${app.get('port')}`);
 });
