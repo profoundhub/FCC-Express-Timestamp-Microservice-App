f2b08385acd333b2a7111881ae2393c757d32a38
Daniel Lim <hbt@markhamboards.com>
2016-09-11 00:22:39 -0400

    getting there
    getting there

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 "use strict";
-
 const express = require('express');
 const app = express();
-
 const http = require('http').Server(app);
 const path = require('path');
-
-const moment = require('moment');
-
 // app.set('port', (process.env.PORT || 8081));
 const port = process.env.PORT || 8081;
+const moment = require('moment');
 
-app.get('/', function (req, res) {
-   res.send('Test App!');
+app.get('/:date', function (req, res) {
+   // res.send('Test App!');
    var queryDate = new Date(req.params.date);
    console.log(queryDate);
 
-if (queryDate !== "Invalid Data") {
+if (queryDate !== "Invalid Date") {
   var unixDate = moment.unix(queryDate);
   var humanDate = moment(queryDate);
 
@@ -34,6 +30,6 @@ http.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
 */
-  console.log("Example App Listening on *:" + port);
+  console.log("TimeStamp API App Listening on *:" + port);
 
 })
